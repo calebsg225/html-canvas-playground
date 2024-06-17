3ebdd8e2717cd0aa01b02d6c2c6baf5b811b4fe7
caleb <calebsg2253284@gmail.com>
2024-06-17 03:00:09 -0400

    remove compiled ts

--- src/scripts/js/chess3p.js
+++
@@ -1,6 +0,0 @@
-const chess3p = document.createElement('canvas');
-const ctx = chess3p.getContext("2d");
-ctx.fillStyle = "blue";
-ctx.fillRect(10, 10, 100, 100);
-document.body.append(chess3p);
-//# sourceMappingURL=chess3p.js.map
