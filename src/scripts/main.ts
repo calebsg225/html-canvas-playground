bbb1ccab1c85972c869751c68800db1fdeaad1bc
calebsg225 <calebsg2253284@gmail.com>
2025-05-13 21:49:34 -0400

    add 2player chess board to main.ts

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,12 @@
+import Chess2Player from "./chess2p.js";
 import Chess3PLayer from "./chess3p.js";
 import Chess4Player from "./chess4p.js";
 
-const i = new Chess4Player(`4pchess`, document.body);
-i.changeChessVariable('imageResolution', 1000);
+const i = new Chess2Player(`2pchess`, document.body);
+i.changeChessVariable('imageResolution', 1024);
 
 const j = new Chess3PLayer(`3pchess`, document.body);
-j.changeChessVariable('imageResolution', 1000);
+j.changeChessVariable('imageResolution', 1024);
+
+const k = new Chess4Player(`4pchess`, document.body);
+k.changeChessVariable('imageResolution', 1024);
